d7ab81fbc9d2c02e773b5cc7d3a1cdc08fe51667
Cássio Abreu <cassioabreu_tri@hotmail.com>
2020-07-04 12:15:20 -0300

    Menu Page funcionando

--- Iteris/src/app/app-routing.module.ts
+++ Iteris/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuPageComponent } from './menu-page/menu-page.component';
+import { ServicosComponent } from './serviços/servicos/servicos.component';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { DetallhesComponent } from './detallhes/detallhes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuPageComponent,
+    children: [
+      {
+        path: '',
+        component: InicioComponent,
+      },
+      {
+        path: 'servicos',
+        component: ServicosComponent,
+      },
+      {
+        path: 'detalhes',
+        component: DetallhesComponent,
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Iteris/src/app/app.module.ts
+++ Iteris/src/app/app.module.ts
@@ -7,18 +7,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { ServicosComponent } from './serviços/servicos/servicos.component';
 import { DetallhesComponent } from './detallhes/detallhes.component';
+import { MenuPageComponent } from './menu-page/menu-page.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     ServicosComponent,
-    DetallhesComponent
+    DetallhesComponent,
+    MenuPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
