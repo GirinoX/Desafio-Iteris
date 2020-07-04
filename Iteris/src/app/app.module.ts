0a98e9e6a12c46318e636e1d5a02f9dac365f9b1
GirinoX <felipe.vismara@gmail.com>
2020-07-04 10:47:17 -0300

    commit Co-authored-by: calabreu <cal.abreu@yahoo.com.br> Co-authored-by: Tiago Vinicius de Mello Tavares <tigger1557@hotmail.com> Co-authored-by: RuanAmaral <ruan.amaral.lemos@gmail.com>

--- Iteris/src/app/app.module.ts
+++ Iteris/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
