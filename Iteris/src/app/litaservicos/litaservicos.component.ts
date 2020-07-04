c9a1f1053fa5ea32af2ae5e0de2b6874781d74cb
Ruan <ruan.amaral.lemos@gmail.com>
2020-07-04 16:00:48 -0300

    Funcial

--- Iteris/src/app/litaservicos/litaservicos.component.ts
+++ Iteris/src/app/litaservicos/litaservicos.component.ts
@@ -7,32 +7,23 @@ import { Isolamentomodel } from 'src/app/service/servicos/isolamentomodel';
   styleUrls: ['./litaservicos.component.css'],
 })
 export class LitaservicosComponent implements OnChanges {
+
   @Input() servico: Isolamentomodel;
 
-<<<<<<< HEAD
+
   location:String;
   name:String;
-  photo:String = "https://images.generated.photos/qmdENySIv23bkva-PxTHsoxVbZQdB1Wka0ZPcH5shHY/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yy/XzAzMDE4MzAuanBn.jpg";
+  photo:String;
   services:String[];
 
   constructor() { }
-=======
-  location: String;
-  name: String;
-  photo: String;
-  services: String[];
->>>>>>> fabeb270c8aab21c2e4dbe107bc0e8ddc8717173
-
-  constructor() {}
 
   ngOnChanges(): void {
   if (this.servico != null){
-
     this.location=this.servico.location;
     this.name=this.servico.name;
     this.photo=this.servico.photo;
     this.services=this.servico.services;
   }
-  }
-
+}
 }
